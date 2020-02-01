434db86b05a22d5792138d093a1c9ddcec6637b6
unknown <garreffd@uwindsor.ca>
2020-01-31 22:00:27 -0500

    add initial spring boot files

---
+++ src/main/java/com/domgarr/tacoloco/TacoLocoApplication.java
@@ -0,0 +1,13 @@
+package com.domgarr.tacoloco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TacoLocoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TacoLocoApplication.class, args);
+	}
+
+}
